10ce5c8833d53fb696d986d5948d9f61949fdc5f
TeoDawn <teodawn.td@gmail.com>
2024-04-20 23:50:22 -0600

    js to show feedback to user on form submit

---
+++ library/static/js/coment-form.js
@@ -0,0 +1,30 @@
+let submitButton = document.querySelector(".coment-form input[type=submit]");
+let feedback = document.querySelector(".submit_feedback");
+let form_coment = document.querySelector(".coment-form textarea");
+
+function setFeedback(message, color) {
+    feedback.style.visibility = "visible";
+    feedback.style.color = color;
+    feedback.innerHTML = message;
+}
+
+let seconds = 4 * 1000;
+
+submitButton.addEventListener("click", function (event) {
+    // prevents the submit from triggering to quickly and not allowing the feedback to be shown
+    event.preventDefault();  
+
+    if (form_coment.value === "") {
+        setFeedback("Please write a comment", "red");
+        setTimeout(function () {
+            feedback.style.visibility = "hidden";
+        }, seconds);
+    } else {
+        setFeedback("¡Muchas gracias por tus comentarios. Los tomaremos en cuenta!", "#4FCFFF");
+        setTimeout(function () {
+            feedback.style.visibility = "hidden";
+            let form = document.querySelector('.coment-form');
+            form.submit();
+        }, seconds);
+    }
+});
