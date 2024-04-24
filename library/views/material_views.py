34a8084067138593af4e7c773dce4de31778d2e9
TeoDawn <teodawn.td@gmail.com>
2024-04-23 21:28:49 -0600

    MR-004 GenericResourceListView handles is extended by other list views

--- library/views/material_views.py
+++ library/views/material_views.py
@@ -8,37 +8,6 @@ from library.utils import session_visit_register
 
 MATERIALS_TEMPLATE_PATH = "library/material_list.html"
 
-
-@session_visit_register
-class MaterialListView(ListView):
-    model = Material
-    template_name = MATERIALS_TEMPLATE_PATH
-
-    def setup(self, request, *args, **kwargs):
-        print(request.COOKIES)
-        return super().setup(request, *args, **kwargs)
-
-    def get_queryset(self):
-        course = Course.objects.get(slug=self.kwargs["slug"])
-        unit = Unit.objects.get(unit_num=self.kwargs["unit_num"],
-                                course_id=course.id)
-        materials = Material.objects.filter(unit_id=unit.pk)
-        return materials
-
-
-@session_visit_register
-class ExerciseListView(ListView):
-    model = Resource
-    template_name = MATERIALS_TEMPLATE_PATH
-
-    def get_queryset(self):
-        course = Course.objects.get(slug=self.kwargs["slug"])
-        unit = Unit.objects.get(unit_num=self.kwargs["unit_num"],
-                                course_id=course.id)
-        exercises = Resource.objects.filter(unit_id=unit.pk, type='Exercise')
-        return exercises
-
-
 @session_visit_register
 class GenericResourceListView(ListView):
     model = Resource
@@ -54,14 +23,42 @@ class GenericResourceListView(ListView):
                                 course_id=course.id)
         resources = Resource.objects.filter(unit_id=unit.pk, type='Generic')
         return resources
+    
 
+@session_visit_register
+class MaterialListView(GenericResourceListView):
+    model = Material
+    template_name = MATERIALS_TEMPLATE_PATH
 
+    def setup(self, request, *args, **kwargs):
+        print(request.COOKIES)
+        return super().setup(request, *args, **kwargs)
+
+    def get_queryset(self):
+        course = Course.objects.get(slug=self.kwargs["slug"])
+        unit = Unit.objects.get(unit_num=self.kwargs["unit_num"],
+                                course_id=course.id)
+        materials = Material.objects.filter(unit_id=unit.pk)
+        return materials
+
+@session_visit_register
+class ExerciseListView(GenericResourceListView):
+    model = Resource
+    template_name = MATERIALS_TEMPLATE_PATH
+
+    def get_queryset(self):
+        course = Course.objects.get(slug=self.kwargs["slug"])
+        unit = Unit.objects.get(unit_num=self.kwargs["unit_num"],
+                                course_id=course.id)
+        exercises = Resource.objects.filter(unit_id=unit.pk, type='Exercise')
+        return exercises
 
 class MaterialDetailView(DetailView):
     model = Material
     template_name = MATERIALS_TEMPLATE_PATH
 
     def get_object(self):
+        print('context' )
         material = Material.objects.get(id=self.kwargs['material_id'])
         return material
 
