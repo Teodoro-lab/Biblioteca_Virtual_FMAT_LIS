6dd4f8e75c0d84fd5ce44c38b67a3c7ae68fbf1c
TeoDawn <teodawn.td@gmail.com>
2024-05-06 01:35:55 -0600

    MR-006 Added MinValue to 0 validation for User quotes

--- accounts/models.py
+++ accounts/models.py
@@ -8,8 +8,8 @@ DAILY_COMMENT_QUOTA = 10
 
 class CustomUser(AbstractUser):
     email = models.EmailField(max_length=254, unique=True)
-    comment_quota = models.IntegerField(default=DAILY_COMMENT_QUOTA)
-    file_quota = models.IntegerField(default=DAILY_FILE_QUOTA)
+    comment_quota = models.IntegerField(default=DAILY_COMMENT_QUOTA, validators=[MinValueValidator(0)])
+    file_quota = models.IntegerField(default=DAILY_FILE_QUOTA, validators=[MinValueValidator(0)])
     last_reset = models.DateTimeField(default=timezone.now)
 
     def reset_form_quota(self):
