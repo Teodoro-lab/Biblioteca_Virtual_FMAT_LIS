a19cd06e7b8c3e07b034d8c3d5518d9a1eae5890
TeoDawn <teodawn.td@gmail.com>
2024-05-08 12:18:31 -0600

    MR-007 fix: css for microsoft login button not being applied due missing config

--- fmat_virtual_library/settings.py
+++ fmat_virtual_library/settings.py
@@ -151,6 +151,9 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
