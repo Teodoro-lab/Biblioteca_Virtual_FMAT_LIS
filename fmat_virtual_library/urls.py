86f3e83cab44a910116dfe8a10da5e5037a25822
TeoDawn <teodawn.td@gmail.com>
2024-05-08 12:04:31 -0600

    MR-007 Added ms identity web library URLs to perform the OAuth

--- fmat_virtual_library/urls.py
+++ fmat_virtual_library/urls.py
@@ -3,12 +3,17 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+from ms_identity_web.django.msal_views_and_urls import MsalViews
+
 from RestLibrary.urls import router as api_router
+
+msal_urls = MsalViews(settings.MS_IDENTITY_WEB).url_patterns()
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/", include("accounts.urls")),
-    path("", include("library.urls")),
+    path("", include("library.urls"), name="index"),
     path("api/", include(api_router.urls)),
+    path(f'{settings.AAD_CONFIG.django.auth_endpoints.prefix}/', include(msal_urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
